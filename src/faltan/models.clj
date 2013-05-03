d522b0eb72a382423d81f249b8ecca1d8a863042
alvare <welcometothechango@gmail.com>
2013-05-03 17:21:27 -0300

    db config

--- src/faltan/models.clj
+++ src/faltan/models.clj
@@ -11,7 +11,7 @@
            :user "postgres"
            :password "chango"}))
 
-(println (System/getenv "DATABASE_URL") db)
+(println (or (System/getenv "HEROKU_POSTGRESQL_SILVER_URL") db))
 
 (defn all []
   (sql/with-connection (System/getenv "DATABASE_URL")
