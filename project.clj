d5af3571e431f7cedad3785ea62afc78126109b8
alvare <welcometothechango@gmail.com>
2013-05-03 17:04:00 -0300

    environment fixes

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql "9.1-901.jdbc4"]
                  [clj-time "0.5.0"]
-                 [clj-json "0.5.3"]]
+                 [clj-json "0.3.2"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
